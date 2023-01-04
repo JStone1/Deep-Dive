d1f5b0939cbdd4ad47d1c3d2950173c3d0290757
JStone1 <62293789+JStone1@users.noreply.github.com>
2023-01-04 19:43:50 +0000

    reformatted setup and penguin moving on scroll

--- code.js
+++ code.js
@@ -8,7 +8,18 @@ import { GLTFLoader } from "./vendor_mods/three/examples/jsm/controls/GLTFLoader
 
 gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
 
+let size = { width: 0, height: 0 };
+
 const scene = new THREE.Scene(); // create scene
+scene.background = new THREE.Color("lightblue");
+
+// create renderer
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+});
+
+const container = document.querySelector(".canvas-container");
+container.appendChild(renderer.domElement);
 
 // create camera
 const camera = new THREE.PerspectiveCamera(
@@ -18,25 +29,24 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-// create renderer
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#background"),
-  alpha: true,
-});
+camera.position.set(0, 0, 5); // adjust camera position
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight); // set renderer to size of browser
-document.body.appendChild(renderer.domElement);
+// code to resize canvas to fit the screen
+const onResize = () => {
+  size.width = container.clientWidth;
+  size.height = container.clientHeight;
 
-// code to create basic cube
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-// scene.add(cube);
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
 
-camera.position.z = 5; // adjust camera position
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+};
 
-const controls = new OrbitControls(camera, renderer.domElement); // create controls to move around the scene
+window.addEventListener("resize", onResize);
+onResize();
+
+const controls = new OrbitControls(camera, container); // create controls to move around the scene
 
 // add basic pointlight and set the position
 const directionalLight = new THREE.DirectionalLight(0xffffff);
@@ -44,37 +54,94 @@ directionalLight.position.set(0, 5, 5);
 scene.add(directionalLight);
 
 // adds in grid and light helpers to help visualise the scene
-const lightHelper = new THREE.DirectionalLightHelper(directionalLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+// const lightHelper = new THREE.DirectionalLightHelper(directionalLight);
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper);
 
-// initialise GLTF loader
-const loader = new GLTFLoader();
+// // initialise GLTF loader
+// const loader = new GLTFLoader();
 
-let penguinModel;
-
-// load in penguin model
-loader.load(
-  "./models/penguinTest3.glb",
-  function (gltf) {
-    gltf.scene.scale.set(0.6, 0.6, 0.6); // scales the model before adding to scene
-    penguinModel = gltf;
-    scene.add(gltf.scene);
-  },
-  undefined,
-  function (error) {
-    console.error(error);
-  }
-);
+// let penguinModel;
+// // load in penguin model
+// loader.load(
+//   "./models/penguinTest3.glb",
+//   function (gltf) {
+//     gltf.scene.scale.set(0.6, 0.6, 0.6); // scales the model before adding to scene
+//     penguinModel = gltf;
+//     scene.add(gltf.scene);
+//   },
+//   undefined,
+//   function (error) {
+//     console.error(error);
+//   }
+// );
 
 // animation loops that renders scene to the screen
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 
-  // penguinModel.scene.rotation.x += 0.5;
-
   controls.update();
 }
 
 animate();
+
+// array that stores objects to be loaded into the screen with name, group and filepath
+const toLoad = [
+  {
+    name: "penguin",
+    group: new THREE.Group(),
+    file: "./models/penguinTest3.glb",
+  },
+];
+
+// object to store models
+const models = {};
+
+const setupAnimation = () => {
+  console.log("Setup animation");
+  models.penguin.position.x = 15;
+  ScrollTrigger.matchMedia({
+    "(prefers-reduced-motion: no-preference)": desktopAnimation,
+  });
+};
+
+const desktopAnimation = () => {
+  let section = 0;
+  const tl = new gsap.timeline({
+    defaults: {
+      duration: 1,
+      ease: "power2.inOut",
+    },
+    scrollTrigger: {
+      trigger: ".page",
+      start: "top top",
+      end: "bottom bottom",
+      scrub: 0.1,
+    },
+  });
+  console.log("here", models.penguin);
+  tl.to(models.penguin.position, { x: 1 }, section);
+};
+
+const LoadingManager = new THREE.LoadingManager(() => {
+  console.log("Loading manager");
+  setupAnimation();
+});
+
+const gltfLoader = new GLTFLoader(LoadingManager);
+
+toLoad.forEach((item) => {
+  gltfLoader.load(item.file, (model) => {
+    model.scene.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        console.log("Child", child);
+        child.receiveShadow = true;
+        child.castShadow = true;
+      }
+    });
+    item.group.add(model.scene);
+    scene.add(item.group);
+    models[item.name] = item.group;
+  });
+});
